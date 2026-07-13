ec51eece036c7095321a83383c89bd12b912b7de
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-13 22:22:02 +0530

    basic arithmatic\

---
+++ basics/arithmatic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int a = 10;
+  int b = 20; // double use kro for decimals
+  std::cout << a + b << std::endl;
+  std::cout << a - b << std::endl;
+  std::cout << a++ << std::endl; // post increment
+  std::cout << a-- << std::endl; // pre decrement
+  std::cout << a * b << std::endl;
+  std::cout << a * b << std::endl;
+  std::cout << a / b << std::endl;
+  std::cout << a % b << std::endl;
+  return 0;
+}
