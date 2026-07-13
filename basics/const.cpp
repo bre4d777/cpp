9233de3665e16f230deb570c38748be9f30971ee
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-13 21:22:25 +0530

    constants

---
+++ basics/const.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  const double PI = 3.14;
+  int radius = 10;
+  double area = PI * radius * radius;
+  std::cout << "lele area: " << area << std::endl;
+  return 0;
+}
