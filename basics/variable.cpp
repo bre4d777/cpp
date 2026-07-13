9d6262a3b413575d7bc91477bbc7258e31c87740
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-13 21:17:30 +0530

    variables and basic data types

---
+++ basics/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int l = 69;                  // integer
+  double pi = 3.14;            // double (allows decimal)
+  char c = 'a';                // single character
+  bool b = true;               // boolean (true or false)
+  std::string s = "lund lele"; // string
+  std::cout << l << "\n" << pi << "\n" << c << "\n" << b << "\n" << s << "\n";
+  return 0;
+}
