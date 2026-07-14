6206e7114594e064e22d9c25652b78119aef46b3
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-14 16:37:54 +0530

    switches

---
+++ basics/switches.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int gf;
+  std::cout << "gf: ";
+  std::cin >> gf;
+  switch (gf) {
+  case 0:
+    std::cout << "randwa" << std::endl;
+    break;
+  case 1:
+    std::cout << "ladkibaaz" << std::endl;
+    break;
+  default:
+    std::cout << "manwhore" << std::endl;
+    break;
+  }
+  return 0;
+}
