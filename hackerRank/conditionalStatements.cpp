7f93a4530784cc7b34ceeb26288665741b1a0872
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-15 15:00:14 +0530

    .

---
+++ hackerRank/conditionalStatements.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  if (a == 1) {
+    cout << "one\n";
+    return 0;
+  } else if (a == 2) {
+    cout << "two\n";
+    return 0;
+  } else if (a == 3) {
+    cout << "three\n";
+    return 0;
+  } else if (a == 4) {
+    cout << "four\n";
+    return 0;
+  } else if (a == 5) {
+    cout << "five\n";
+    return 0;
+  } else if (a == 6) {
+    cout << "six\n";
+    return 0;
+  } else if (a == 7) {
+    cout << "seven\n";
+    return 0;
+  } else if (a == 8) {
+    cout << "eight\n";
+    return 0;
+  } else if (a == 9) {
+    cout << "nine\n";
+    return 0;
+  } else if (a > 9) {
+    cout << "Greater than 9\n";
+    return 0;
+  }
+  return 0;
+}
