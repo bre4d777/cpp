e3a379f8648f3e86b5afe5fefb8f1b3bba1f58dc
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-15 17:19:06 +0530

    nested loops

---
+++ basics/nestedLoop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      std::cout << i << " lund " << j << std::endl;
+    }
+  }
+  return 0;
+}
