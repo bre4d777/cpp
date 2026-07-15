751efb5979fdf7c07d3de72c38254f8d9dfaae83
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-15 17:52:48 +0530

    something

--- basics/userDefinedFunc.cpp
+++ basics/userDefinedFunc.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
-void printNum(int num) { std::cout << num << std::endl; }
-
-void getNum(int &num) {
-  std::cout << "Enter a number: ";
-  std::cin >> num;
-}
+void getNum(int &num);
+void printNum(int num);
 
 int main() {
   int num;
   getNum(num);
   printNum(num);
   return 0;
+}
+
+void printNum(int num) { std::cout << num << std::endl; }
+
+void getNum(int &num) {
+  std::cout << "Enter a number: ";
+  std::cin >> num;
 }
