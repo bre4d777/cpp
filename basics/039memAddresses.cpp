54df908c15a41746487a47ca64bc0fcf2789e4c9
bre4d777 <bre4d777@users.noreply.github.com>
2026-07-16 22:04:22 +0530

    mem add

---
+++ basics/039memAddresses.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string s = "s";
+  int num = 10;
+  bool b = true;
+  cout << &s << endl;
+  cout << &num << endl;
+  cout << &b << endl;
+  return 0;
+}
